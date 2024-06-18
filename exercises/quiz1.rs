f1dc4c39964132cbb11d6544071d8a7e71167717
tolu <95189430+tolu0x@users.noreply.github.com>
2024-06-18 19:10:39 +0100

    completed first quiz

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,20 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_amount: u32) -> u32 {
+    let mut price: u32 = 2;
+
+    if apple_amount > 40 {
+        price = 1;
+    } else {
+        price = 2;
+    }
+
+    price * apple_amount
+    
+}
 
 // Don't modify this function!
 #[test]
